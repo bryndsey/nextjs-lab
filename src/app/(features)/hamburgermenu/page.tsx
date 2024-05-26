cb5ab5cd15092794e1e9ec3abc0f9f237de9687f
Bryan <blinz117@gmail.com>
2024-05-26 13:47:11 -0500

    Remove unneeded border

--- src/app/(features)/hamburgermenu/page.tsx
+++ src/app/(features)/hamburgermenu/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
     <ThreeLayout>
       <main className="flex min-h-screen flex-col items-center p-24">
         <h1 className="text-3xl font-bold text-center">Hamburger menu</h1>
-        <HamburgerButton className="h-44 w-44 border" />
+        <HamburgerButton className="h-44 w-44" />
       </main>
     </ThreeLayout>
   );
